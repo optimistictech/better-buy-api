835692b94318acac89de5bbf4dd8add228989af2
optimistictech <olaorosamson3@gmail.com>
2021-07-01 09:47:27 +0100

    connection repair

--- includes/Database.php
+++ includes/Database.php
@@ -3,14 +3,13 @@ define('SERVER', $_SERVER['SERVER_NAME']);
 
 if (SERVER != 'localhost' AND SERVER != '127.0.0.1' ) {
    
-define('HOST', 'better-buy-api.herokuapp.com');
-define('USER_NAME', 'optimist_api');
-define('PASSWORD', 'optimistic_api');
-define('DB_NAME', 'optimist_api');
-
+    define('HOST', '54.36.110.215');
+    define('USER_NAME', 'root');
+    define('PASSWORD', '');
+    define('DB_NAME', 'api');
 }else{
    
-define('HOST', 'localhost');
+define('HOST', '54.36.110.215');
 define('USER_NAME', 'root');
 define('PASSWORD', '');
 define('DB_NAME', 'api');

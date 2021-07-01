6a222714f7db8464f7bfad5e695bc9bb15ed612e
optimistictech <olaorosamson3@gmail.com>
2021-07-01 10:21:04 +0100

    Update Database

--- includes/Database.php
+++ includes/Database.php
@@ -3,7 +3,7 @@ define('SERVER', $_SERVER['SERVER_NAME']);
 
 if (SERVER != 'localhost' AND SERVER != '127.0.0.1' ) {
    
-define('HOST', '54.36.110.215');
+define('HOST', 'better-buy-api.herokuapp.com');
 define('USER_NAME', 'optimist_api');
 define('PASSWORD', 'optimistic_api');
 define('DB_NAME', 'optimist_api');
